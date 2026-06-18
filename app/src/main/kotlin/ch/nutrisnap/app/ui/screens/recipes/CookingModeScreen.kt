6c5c2806b00989c507c9875a94a3202c8f835226
sondereggerseverin <sondereggerseverin@gmail.com>
2026-06-18 22:22:29 +0200

    fix: remove duplicate LocalContext import in CookingModeScreen

--- app/src/main/kotlin/ch/nutrisnap/app/ui/screens/recipes/CookingModeScreen.kt
+++ app/src/main/kotlin/ch/nutrisnap/app/ui/screens/recipes/CookingModeScreen.kt
@@ -18,7 +18,6 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ch.nutrisnap.app.data.model.Recipe
-import androidx.compose.ui.platform.LocalContext
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
