bb48db72f96e79fc830ec8af9cc7f09b292ffadd
sondereggerseverin <sondereggerseverin@gmail.com>
2026-06-23 14:21:20 +0200

    feat: add AuthRepository for Supabase email auth

---
+++ app/src/main/kotlin/ch/nutrisnap/app/data/supabase/AuthRepository.kt
@@ -0,0 +1,32 @@
+package ch.nutrisnap.app.data.supabase
+
+import io.github.jan.supabase.auth.auth
+import io.github.jan.supabase.auth.providers.builtin.Email
+import io.github.jan.supabase.auth.user.UserInfo
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+object AuthRepository {
+    private val auth get() = SupabaseClient.client.auth
+
+    val currentUser: UserInfo? get() = auth.currentUserOrNull()
+    val isLoggedIn: Boolean get() = currentUser != null
+
+    suspend fun signUp(email: String, password: String) {
+        auth.signUpWith(Email) {
+            this.email = email
+            this.password = password
+        }
+    }
+
+    suspend fun signIn(email: String, password: String) {
+        auth.signInWith(Email) {
+            this.email = email
+            this.password = password
+        }
+    }
+
+    suspend fun signOut() {
+        auth.signOut()
+    }
+}
