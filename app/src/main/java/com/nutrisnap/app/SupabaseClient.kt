00fe33920b6ffce945da6df9ed2c8aa13bcee5bf
sondereggerseverin <sondereggerseverin@gmail.com>
2026-06-24 16:24:31 +0200

    Add app/src/main/java/com/nutrisnap/app/SupabaseClient.kt

---
+++ app/src/main/java/com/nutrisnap/app/SupabaseClient.kt
@@ -0,0 +1,15 @@
+package com.nutrisnap.app
+
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.gotrue.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseClient {
+    val client = createSupabaseClient(
+        supabaseUrl = BuildConfig.SUPABASE_URL,
+        supabaseKey = BuildConfig.SUPABASE_ANON_KEY
+    ) {
+        install(Auth)
+        install(Postgrest)
+    }
+}
