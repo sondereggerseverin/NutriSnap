5018f0c5c2c444aa3d8e6b8086514555764d9735
sondereggerseverin <sondereggerseverin@gmail.com>
2026-06-24 20:56:51 +0200

    fix: add BuildConfig import from ch.nutrisnap.app namespace

--- app/src/main/java/com/nutrisnap/app/SupabaseClient.kt
+++ app/src/main/java/com/nutrisnap/app/SupabaseClient.kt
@@ -1,5 +1,6 @@
 package com.nutrisnap.app
 
+import ch.nutrisnap.app.BuildConfig
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.postgrest.Postgrest
